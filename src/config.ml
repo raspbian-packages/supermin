97ae89e5522c3d20afd3237e0953c76575925f78
Hilko Bengen <bengen@debian.org>
2022-03-05 09:14:07 +0000

    Import supermin_5.2.2.orig.tar.gz
    [dgit import orig supermin_5.2.2.orig.tar.gz]

---
+++ src/config.ml
@@ -0,0 +1,45 @@
+(* supermin 5
+ * Copyright (C) 2009-2014 Red Hat Inc.
+ * src/config.ml.  Generated from config.ml.in by configure.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *)
+
+let package_name = "supermin"
+let package_version = "5.2.2"
+let host_cpu = "x86_64"
+
+let apt_get = "no"
+let cpio = "/usr/bin/cpio"
+let dnf = "/usr/bin/dnf"
+let dpkg = "/usr/bin/dpkg"
+let dpkg_deb = "/usr/bin/dpkg-deb"
+let dpkg_query = "/usr/bin/dpkg-query"
+let dpkg_divert = "/usr/bin/dpkg-divert"
+let fakeroot = "/usr/bin/fakeroot"
+let makepkg = "no"
+let pacman = "no"
+let pactree = "no"
+let pacman_g2 = "no"
+let rpm = "/usr/bin/rpm"
+let rpm2cpio = "/usr/bin/rpm2cpio"
+let urpmi = "no"
+let yumdownloader = "/usr/bin/yumdownloader"
+let xzcat = "/usr/bin/xzcat"
+let zcat = "/usr/bin/zcat"
+let zypper = "no"
+
+let mke2fs = "/usr/sbin/mke2fs"
+let mke2fs_t_option = "-t"
